ab8e87b2c0d88930005d0bd0b3fcb1801f26af04
JorgeFigueroa <atientas0412@gmail.com>
2019-01-24 18:10:17 +0100

    docker virtualbox

--- docker.sh
+++ docker.sh
@@ -2,7 +2,7 @@
 docker --version
 docker container --help
 
-docker run --interactive --tty ubuntu bash  #crea container ubuntu
+docker run -p 5432:5432  --name bmw-db -e POSTGRES_PASSWORD=postgres -d postgres:9.3 #crea container con image postgress
 
 docker container ls --all          ## list container
 docker ps -a                       ## list container
@@ -13,11 +13,26 @@ docker commit CONTAINER_ID  NAME
 
 docker run --name REPOSITORY -d test/nginx ##crea images
 
-docker run -p 5432:5432  --name bmw-db -e POSTGRES_PASSWORD=postgres -d postgres:9.3
-docker run --name dbTest -e POSTGRES_PASSWORD=postgres -d postgres
+
+
 docker cp filename nomecontainer:percorsoDoveCopiare
 docker exec -it nomeContainerPostgres bash psql -d database -U username -f percorsoDoveCopiare/fileDump.sql
+docker exec -it CONTAINER_ID  bash
 
 
- docker login
- docker push
+docker build -t friendlyhello .  # Create image using this directory's Dockerfile
+docker run -p 4000:80 friendlyhello  # Run "friendlyname" mapping port 4000 to 80
+docker run -d -p 4000:80 friendlyhello         # Same thing, but in detached mode
+docker container ls                                # List all running containers
+docker container ls -a             # List all containers, even those not running
+docker container stop <hash>           # Gracefully stop the specified container
+docker container kill <hash>         # Force shutdown of the specified container
+docker container rm <hash>        # Remove specified container from this machine
+docker container rm $(docker container ls -a -q)         # Remove all containers
+docker image ls -a                             # List all images on this machine
+docker image rm <image id>            # Remove specified image from this machine
+docker image rm $(docker image ls -a -q)   # Remove all images from this machine
+docker login             # Log in this CLI session using your Docker credentials
+docker tag <image> username/repository:tag  # Tag <image> for upload to registry
+docker push username/repository:tag            # Upload tagged image to registry
+docker run username/repository:tag                   # Run image from a registry
--- virtualBox.sh
+++ virtualBox.sh
@@ -1,6 +1,3 @@
 "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" list hdds
-VBoxManage clonehd box-disk1.vmdk box-disk1.vdi --format vdi
-VBoxManage.exe clonehd “C:\path\ubuntu.vmdk” “C:\path\cloned_ubuntu.vdi” — format vdi
-
+VBoxManage.exe clonehd “C:\path\disco.vmdk” “C:\path\cloned_disco.vdi” — format vdi
 VBoxManage.exe modifyhd “C:\path\cloned_ubuntu.vdi” — resize 24576
-VBoxManage modifyhd box-disk1.vdi --resize 10240 
