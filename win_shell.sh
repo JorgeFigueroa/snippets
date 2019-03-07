e8d80ca00c88d3675208a199fe96c8daa810805e
JorgeFigueroa <atientas0412@gmail.com>
2019-03-07 15:29:07 +0100

    win shell

--- win_shell.sh
+++ win_shell.sh
@@ -1,1 +1,4 @@
-wmic process get processid,parentprocessid,executablepath | find "PROCESS"   #Find a Windows process based on its description, using CMD
+wmic process get processid,parentprocessid,executablepath | find "nameProcess"   #Find a Windows process based on its description, using CMD
+tasklist /v | find "nameProcess"    #Find a Windows process based on its description, using CMD
+
+taskkill /F /PID  processId     #Kill process
