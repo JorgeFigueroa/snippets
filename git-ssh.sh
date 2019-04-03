d3018f0eaa8e0c6afb29fb31730dcac94e63d4a9
jorge figueroa <atientas0412@gmail.com>
2019-04-04 00:30:35 +0200

    ssh git

--- git-ssh.sh
+++ git-ssh.sh
@@ -1,6 +1,8 @@
 ls -al ~/.ssh-keygen 		##see if existing SSH keys are present
-ssh-keygen -t rsa -b 4096 -C "your_email@example.com"	##Generating a new SSH key
+ssh-keygen -t rsa -b 4096 -C "my@gmail.com" -f  /Users/jorge/.ssh/	##Generating a new SSH key
+ssh -i /path/to/key -vT git@github.com
 eval "$(ssh-agent -s)"		##Start the ssh-agent in the background.
 ssh-add ~/.ssh/id_rsa 		##Add your SSH private key to the ssh-agent and store your passphrase in the keychain
-pbcopy < ~/.ssh/id_rsa.pub. ##Copies the contents of the id_rsa.pub
+ssh -T git@github.com       #Hi USER! You've successfully authenticated
+pbcopy < ~/.ssh/id_rsa.pub  ##Copies the contents of the id_rsa.pub
 
