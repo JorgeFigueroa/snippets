48c01a0c608ee5d6d48f50abebe91e1a7d3b73db
jorge figueroa <atientas0412@gmail.com>
2019-05-07 22:12:03 +0200

    mongo

--- mongo.sh
+++ mongo.sh
@@ -1,43 +1,18 @@
-$mongod     
-$mongo
-127.0.0.1:27017
-
->help
->db.help
->db.help()
->db.stats()   stadisticas
+$mongod     $mongo     #127.0.0.1:27017
+help
+db.help
+db.help()
 exit
->show dbs
->use miPrimeraDB  // crea db
-
-# edicion insertar buscar
+show dbs
 show collections
-db.collection.insert({nombre:”jorge”})  // crea collection
-db.collection.find().pretty()
-db.system.indexes.find()
 
-#edicion borrar adaptar
-db.collection.remove({edad:28})
-use miPrimeraDB db.dropDatabase()
+use miPrimeraDB   #crea db
+use miPrimeraDB.db.dropDatabase()
 
-
-
-
-#incrementar e decremetar un campo numerico
-db.collection.update(  {campo1:”aaa”},  {campo2 : “bbb”}  ) db.collection.update(  {nombre:”aaa”},   {$set: {apellido: “cccc”, edad 20}})
-db.collection.update(  {campo:”valor”}, {$inc: { edad :1 }})
-db.collection.update(  {campo:”valor”}, {$inc: { edad :-1 }})
-db.collection.update(  {},   {$inc: { edad : 1 }},   false , true) // todos 
-
-
-
-
-
-
-Mongo	
 tail -f /var/log/mongodb/mongod.log
 
 mongodump --query '{"key" : "value"}' --db <dbName> --collection <collectionName>
 mongodump --db <dbName> --collection <collectionName> --queryFile query.json
+mongorestore --drop dump_mongo-static		 #cancella vechio mongo e ricarica nuovo  db mongo
 
-mongorestore --drop dump_mongo-static		 // cancella vechio mongo e ricarica nuovo  db mongo
+
