eec88acd960f9fac9cca5d2089dc590ecf64df18
jorge figueroa <atientas0412@gmail.com>
2019-05-18 13:02:27 +0200

    js

--- js_condicionales_ciclo.js
+++ js_condicionales_ciclo.js
@@ -14,10 +14,8 @@ if (false) {
 
 //boleanos -> true / false
 /* operadores logicos
-> mayor que
-< menor que
-|| OR
-&& AND
+> mayor   < menor 
+|| OR  && AND
 == === igual
 != !== diferente
 !  no
@@ -55,3 +53,39 @@ do {
   if (contador >= 10)
     break; // rompe el ciclo termina el ciclo
 } while (true);
+
+
+
+//modulo incremento decremento
+//    %   ++     --
+
+function espar(numero) {
+  if(numero % 2 == 0) 
+    return ' es multiplo 2 ';
+  return ' no es multiplo de 2 ';
+}
+
+Math;
+//redondeo
+Math.pow(2,3);
+Math.round(34.2);
+Math.ceil(34.2);
+Math.floor(34.2);
+Math.random(); //funcion aleatorio
+Math.sqrt(4);  //raiz cuadrada
+
+
+
+//#####  CADENAS
+var nombre = 'jorge';
+
+if (nombre.indexOf('jorge') === 0)
+
+nombre.indexOf("jorge");
+nombre[4];
+nombre.charAt(1);
+nombre.slice(1,4);
+//mayusculo mminusculo
+nombre.toUpperCase();
+nombre.toLowerCase();
+nombre.replace("j","y");
