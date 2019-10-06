ed14e7df80f463130ac2d0603ea45133ae893612
JorgeFigueroa <atientas0412@gmail.com>
2019-10-06 21:12:08 +0200

    Update postgres.sh

--- postgres.sh
+++ postgres.sh
@@ -1,11 +1,18 @@
+#http://www.postgresqltutorial.com/psql-commands/
 open cmd
 cd C:\Program Files\PostgreSQL\12\bin
 psql --version
-psql -h localhost -d mydb -U myuser -W
+psql -h localhost -d mydb -U myuser -W    #Connect to PostgreSQL database
 psql -d mydb -U myuser
-      dt;
-
-
+ 
 create database mydb;
-create  user name-user  with  password 'pass';
+create  user name_user  with  password 'pass';
 \c mydb
+\c dbname username #connection database
+\l    #List databases
+\dt   #List tables
+
+
+
+
+
