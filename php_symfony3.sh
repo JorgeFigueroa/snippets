0405cd2be015e57e76004a938f03fc56676b73b0
JorgeFigueroa <atientas0412@gmail.com>
2019-10-16 12:51:41 +0200

    Update php_symfony3.sh

--- php_symfony3.sh
+++ php_symfony3.sh
@@ -1,6 +1,7 @@
 ### PHP WIN10
 https://www.forevolve.com/en/articles/2016/10/27/how-to-add-your-php-runtime-directory-to-your-windows-10-path-environment-variable/
 Pannello di controllo\Tutti gli elementi del Pannello di controllo\Sistema
+SET PATH=c:\php\php5.3.29\;%PATH%
 
 ### PHP
 nano ~/.bash_profile
