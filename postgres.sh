e410fc0a26279258740251ccba9b364f947b1b25
JorgeFigueroa <atientas0412@gmail.com>
2019-10-20 21:41:59 +0200

    Update postgres.sh

--- postgres.sh
+++ postgres.sh
@@ -4,10 +4,10 @@ cd C:\Program Files\PostgreSQL\12\bin
 
 pg_dump -U username -C -f D:\path\name.dump name_db  #dump
 create database name_db;
-psql -U postgres -d name_db -1 -f D:\path\name.dump  #restore
+./psql -U postgres -d name_db -1 -f D:\path\name.dump  #restore
 
-psql --version
-psql -h localhost -U myuser -W    #Connect to PostgreSQL database
+./psql --version
+./psql -h localhost -U myuser -W    #Connect to PostgreSQL database
  
 create database mydb;
 create  user name_user  with  password 'pass';
