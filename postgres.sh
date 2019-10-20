b3dad99be896c8d403a7c6755cfa9164003819e9
JorgeFigueroa <atientas0412@gmail.com>
2019-10-20 21:42:44 +0200

    Update postgres.sh

--- postgres.sh
+++ postgres.sh
@@ -7,7 +7,7 @@ create database name_db;
 ./psql -U postgres -d name_db -1 -f D:\path\name.dump  #restore
 
 ./psql --version
-./psql -h localhost -U myuser -W    #Connect to PostgreSQL database
+./psql -h localhost -U postgres -W    #Connect to PostgreSQL database
  
 create database mydb;
 create  user name_user  with  password 'pass';
