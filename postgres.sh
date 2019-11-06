86def298105d9cc245f04457c01b09b6603796f6
JorgeFigueroa <atientas0412@gmail.com>
2019-11-06 17:15:39 +0100

    Update postgres.sh

--- postgres.sh
+++ postgres.sh
@@ -4,12 +4,12 @@ open cmd
 win C:\Program Files\PostgreSQL\12\bin 
 osx  /Library/PostgreSQL/11/bin
 
-pg_dump -U postgres -C -f D:\name.dump name_db  #dump
+pg_dump -h localhost -p 5432 -U postgres -C -f D:\name.dump name_db  #dump
 create database name_db;
-./psql -U postgres -d name_db -1 -f D:\name.dump  #restore
+./psql -h localhost -p 5432 -U postgres -d name_db -1 -f D:\name.dump  #restore
 
 ./psql --version
-./psql -h localhost -U postgres -W    #Connect to PostgreSQL database
+./psql -h localhost -p 5432 -U postgres -W    #Connect to PostgreSQL database
  
 create database mydb;
 create  user name_user  with  password 'pass';
