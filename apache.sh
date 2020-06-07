ad0474ba10daeb0cc948fe987e0056ae32471a0c
JorgeFigueroa <atientas0412@gmail.com>
2020-06-07 13:58:46 +0200

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -1,6 +1,7 @@
 #WIN10
 https://www.apachelounge.com/download/
-https://support.microsoft.com/en-us/help/2977003/the-latest-supported-visual-c-downloads
+https://support.microsoft.com/en-us/help/2977003/the-latest-supported-visual-c-downloads    vc_redist.x64.exe
+
 
 Apache24\bin\httpd.exe    --> http://localhost
 httpd.exe -k install    #install as a service. Open command prompt as Administrator
