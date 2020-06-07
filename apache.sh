33b5a6f3bf9dfd8a05cf8ada3b7efcffaf1dbd64
JorgeFigueroa <atientas0412@gmail.com>
2020-06-07 13:59:25 +0200

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -5,6 +5,6 @@ https://support.microsoft.com/en-us/help/2977003/the-latest-supported-visual-c-d
 
 Apache24\bin\httpd.exe    --> http://localhost
 httpd.exe -k install    #install as a service. Open command prompt as Administrator
-services.msc           #You can start/stop the service:
+services.msc            #You can start/stop the service:
 
-httpd -h               # To see all Command line options:
+httpd -h                # All Command line options:
