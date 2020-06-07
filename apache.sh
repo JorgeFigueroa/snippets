b3deba844069a3fd1f198a31c1ddcef6ba402338
JorgeFigueroa <atientas0412@gmail.com>
2020-06-07 13:55:38 +0200

    Create apache.sh

---
+++ apache.sh
@@ -0,0 +1,9 @@
+#WIN10
+https://www.apachelounge.com/download/
+
+
+Apache24\bin\httpd.exe    --> http://localhost
+httpd.exe -k install    #install as a service. Open command prompt as Administrator
+services.msc           #You can start/stop the service:
+
+httpd -h               # To see all Command line options:
