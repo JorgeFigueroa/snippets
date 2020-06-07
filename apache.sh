d08e97770cfbfb8f8dec6d2346b2080167e78833
JorgeFigueroa <atientas0412@gmail.com>
2020-06-07 13:56:49 +0200

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -1,7 +1,6 @@
 #WIN10
 https://www.apachelounge.com/download/
 
-
 Apache24\bin\httpd.exe    --> http://localhost
 httpd.exe -k install    #install as a service. Open command prompt as Administrator
 services.msc           #You can start/stop the service:
