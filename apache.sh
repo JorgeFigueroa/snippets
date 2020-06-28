ec097ab7046a0886289dba3ad4cf278f799d6f0b
JorgeFigueroa <atientas0412@gmail.com>
2020-06-29 00:39:12 +0200

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -44,6 +44,17 @@ cd Apache24\conf\extra\httpd-vhosts.conf
 </VirtualHost>
 
 
+https://www.linode.com/docs/web-servers/apache/install-and-configure-apache-on-centos-7/
+#### APACHE #httpd.conf modulos y  cors   
+MAMP_headers_module_MAMPLoadModule headers_module modules/mod_headers.so
+MAMP_rewrite_module_MAMPLoadModule rewrite_module modules/mod_rewrite.so
+
+<IfModule mod_headers.c>
+    Header set Access-Control-Allow-Origin "*"
+    Header set Access-Control-Allow-Headers "*"
+    Header set Access-Control-Allow-Methods "GET, POST, PUT, DELETE, OPTIONS"
+</IfModule>
+
 
 
 C:\Windows\System32\drivers\etc\hosts
