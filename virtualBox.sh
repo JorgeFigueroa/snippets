614f55c0c833c05364cbd48959d70fe3bee2bd8f
JorgeFigueroa <atientas0412@gmail.com>
2020-09-30 00:47:19 +0200

    Update virtualBox.sh

--- virtualBox.sh
+++ virtualBox.sh
@@ -1,3 +1,7 @@
+cmd 
+bcdedit
+bcdedit /set hypervisorlaunchtype off
+
 "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" list hdds
 VBoxManage.exe clonehd “C:\path\disco.vmdk” “C:\path\cloned_disco.vdi” —format vdi
 VBoxManage.exe modifyhd “C:\path\cloned_ubuntu.vdi” —resize 24576
