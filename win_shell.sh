eb59c1e139280ab068442f291d4c20b6b34626ac
JorgeFigueroa <atientas0412@gmail.com>
2020-11-28 21:47:12 +0100

    Update win_shell.sh

--- win_shell.sh
+++ win_shell.sh
@@ -1,8 +1,7 @@
 ping www.google.com
 nslookup      #name dominio
 
-
-d:
+d:  #change disk
 
 
 Get-Content D:\log.txt -Tail 3
@@ -15,28 +14,14 @@ tasklist /v | find "nameProcess"    #Find a Windows process based on its descrip
 taskkill /F /PID  processId     #Kill process
 
 
+bootcfg     #Configurare, interrogare o modificare le impostazioni del file Boot.ini.
+driverquery #Visualizzazione di un elenco dei drivers di tutte le periferiche installate e delle loro propriet‡.
+getmac      #Visualizzazione dell'indirizzo MAC (Media Access Control) e di un elenco dei protocolli di rete associati ad ogni indirizzo per tutte le schede di rete in ogni computer.
+gpresult    #Visualizzazione delle impostazioni del Group Policy e dell'RSOP (Resultant Set of Policy) di un utente o di un computer.
 
-
-bootcfg - Configurare, interrogare o modificare le impostazioni del file Boot.ini.
-
-
-driverquery - Visualizzazione di un elenco dei drivers di tutte le periferiche installate e delle loro propriet‡.
-
-
-getmac - Visualizzazione dell'indirizzo MAC (Media Access Control) e di un elenco dei protocolli di rete associati ad ogni indirizzo per tutte le schede di rete in ogni computer.
-
-
-gpresult - Visualizzazione delle impostazioni del Group Policy e dell'RSOP (Resultant Set of Policy) di un utente o di un computer.
-
-
-netsh - Questo comando puÚ essere utilizzato all'interno del "Netsh Interface IP" al fine di configurare il protocollo TCP/IP.
-
-
-schtasks - Schematizzazione dei comandi e dei programmi che girano peridicamente o in un tempo specifico sul computer.
-
-
-
-systeminfo - Visualizzazione di informazioni dettagliate sulla configurazione del computer e del sistema operativo installato su di esso.
+netsh #Questo comando puÚ essere utilizzato all'interno del "Netsh Interface IP" al fine di configurare il protocollo TCP/IP.
+schtasks #Schematizzazione dei comandi e dei programmi che girano peridicamente o in un tempo specifico sul computer.
+systeminfo #Visualizzazione di informazioni dettagliate sulla configurazione del computer e del sistema operativo installato su di esso.
 
 
 
@@ -44,244 +29,109 @@ systeminfo - Visualizzazione di informazioni dettagliate sulla configurazione de
 I comandi base
 
 
-attrib
-Il comando attib visualizza o modifica gli attributi dei file.
-ATTRIB maggiori informazioni (www.parlandosparlando.com/view.php/id_405/lingua_0/whoisit_1)
+attrib  #Il comando attib visualizza o modifica gli attributi dei file.
+cd  #Il comando cd permette di cambiare cartella.
+chdir #Il comando chdir cambia la directory corrente o ne visualizza il nome.
+chkdsk  #Il comando chkdsk verifica il disco e visualizza una relazione sul suo stato.
+cls #Il comando cls cancella il contenuto della schermata.
+compact #Il comando compact permette di comprimere e decomprimere nelle partizioni NTFS i file di una directory, e di rammentare che tutti i file che si aggiungeranno in quella directory andranno compressi o decompressi.
 
 
-cd
-Il comando cd permette di cambiare cartella.
-CD maggiori dettagli (www.parlandosparlando.com/view.php/id_406/lingua_0/whoisit_1)
+copy  #Il comando copy permette la copia di file o gruppi di file.
 
+defrag  #Il comando defrag riorganizza i file su disco per ottimizzarne l'uso.
 
-chdir
-Il comando chdir cambia la directory corrente o ne visualizza il nome.
-CHdIR maggiori dettagli (www.parlandosparlando.com/view.php/id_406/lingua_0/whoisit_1)
 
+del #Il comando del elimina uno o piu' file.
+deltree #Il comando deltree elimina una directory e tutti i file e le sottodirectory in essa contenuti.
+dir #Il comando dir visualizza l'elenco dei file e delle subdirectory di una directory.
 
-chkdsk
-Il comando chkdsk verifica il disco e visualizza una relazione sul suo stato.
-CHKDSK maggiori dettagli (www.parlandosparlando.com/view.php/id_407/lingua_0/whoisit_1)
 
+diskcopy  #Il comando diskcopy copia il contenuto di un disco floppy in un altro disco floppy.
 
-cls
-Il comando cls cancella il contenuto della schermata.
+doskey  #Il comando doskey modifica righe di comando, chiama comandi di MS-DOS e crea macro.
 
 
-command
-Il comando command avvia un nuovo interprete dei comandi MS-DOS.
-COMMAND maggiori dettagli (www.parlandosparlando.com/view.php/id_408/lingua_0/whoisit_1)
+emm386  #Il comando emm386 abilita o disabilita la memoria espansa di EMM386.
 
 
-compact
-Il comando compact permette di comprimere e decomprimere nelle partizioni NTFS i file di una directory, e di rammentare che tutti i file che si aggiungeranno in quella directory andranno compressi o decompressi.
-COMPACT maggiori informazioni (www.parlandosparlando.com/view.php/id_410/lingua_0/whoisit_1)
+expand  #Il comando expand decomprime uno o piu' file compressi.
+fdisk #Il comando fdisk configura un disco rigido per l'utilizzo con MS-DOS.
 
 
-copy
-Il comando copy permette la copia di file o gruppi di file.
-COPY maggiori dettagli (www.parlandosparlando.com/view.php/id_409/lingua_0/whoisit_1)
+format  #Il comando format da' luogo alla formattazione di un'unita'.
 
 
-defrag
-Il comando defrag riorganizza i file su disco per ottimizzarne l'uso.
-DEFRAG maggiori dettagli (www.parlandosparlando.com/view.php/id_411/lingua_0/whoisit_1)
+ipconfig  #Il comando ipconfig permette di conoscere il proprio indirizzo IP, quello del SUBNET MASK e del GATEWAY.
 
+label #Il comando label crea, modifica o elimina l'etichetta di volume di un disco.
 
-del
-Il comando del elimina uno o piu' file.
-DEL maggiori dettagli (www.parlandosparlando.com/view.php/id_412/lingua_0/whoisit_1)
 
+mkdir   #Il comando mkdir crea una nuova directory (cartella).
 
-deltree
-Il comando deltree elimina una directory e tutti i file e le sottodirectory in essa contenuti.
-DelTREE maggiori dettagli ()
 
+more  #Il comando more visualizza i dati di output una schermata per volta.
 
-dir
-Il comando dir visualizza l'elenco dei file e delle subdirectory di una directory.
-DIR maggiori dettagli (www.parlandosparlando.com/view.php/id_413/lingua_0/whoisit_1)
+move  #Il comando move sposta file o cartelle su un nuovo percorso.
 
 
-diskcopy
-Il comando diskcopy copia il contenuto di un disco floppy in un altro disco floppy.
-DISKcOPY maggiori dettagli (www.parlandosparlando.com/view.php/id_414/lingua_0/whoisit_1)
+path  #Il comando path visualizza o imposta un percorso di ricerca per i file eseguibili.
 
 
-doskey
-Il comando doskey modifica righe di comando, chiama comandi di MS-DOS e crea macro.
-DOSKEY maggiori dettagli (www.parlandosparlando.com/view.php/id_415/lingua_0/whoisit_1)
+ping  #Il comando ping consente di verificare la comunicazione tra due computer di una rete.
 
 
-edit
-Il comando edit avvia MS-DOS Editor per creare o modificare i file ASCII.
-EDIT maggiori dettagli (www.parlandosparlando.com/view.php/id_416/lingua_0/whoisit_1)
+print #Il comando print stampa un file di testo consentendo di usare altri comandi di MS-DOS.
 
 
-emm386
-Il comando emm386 abilita o disabilita la memoria espansa di EMM386.
-EmM386 maggiori dettagli (www.parlandosparlando.com/view.php/id_417/lingua_0/whoisit_1)
+prompt  #Il comando prompt cambia il prompt dei comandi di MS-DOS.
 
 
-expand
-Il comando expand decomprime uno o piu' file compressi.
-EXPAND maggiori dettagli (www.parlandosparlando.com/view.php/id_660/lingua_0/whoisit_1)
+rd  #Il comando rd rimuove una directory.
 
 
-fdisk
-Il comando fdisk configura un disco rigido per l'utilizzo con MS-DOS.
-FDISK maggiori dettagli ()
+recover #Il comando recover permette di recuperare documenti andati perduti in settori corrotti del disco fisso (sempre che si abbia un file system NTFS).
 
 
-format
-Il comando format da' luogo alla formattazione di un'unita'.
-FORMAT maggiori dettagli (www.parlandosparlando.com/view.php/id_661/lingua_0/whoisit_1)
+ren #Il comando ren consente di rinominare file o cartelle.
 
 
-ipconfig
-Il comando ipconfig permette di conoscere il proprio indirizzo IP, quello del SUBNET MASK e del GATEWAY.
-IPCONFIG maggiori dettagli (www.parlandosparlando.com/view.php/id_679/lingua_0/whoisit_1)
+rename  #Il comando rename rinomina uno o piu' file.
 
 
-keyb
-Il comando keyb configura la tastiera per la lingua specificata.
-KEYB maggiori dettagli ()
+replace #Il comando replace sostituisce i file.
 
 
-label
-Il comando label crea, modifica o elimina l'etichetta di volume di un disco.
-LABEL maggiori dettagli (www.parlandosparlando.com/view.php/id_662/lingua_0/whoisit_1)
+rmdir #Il comando rmdir rimuove una directory.
 
 
-md
-Il comando md crea una nuova directory (cartella).
-MD maggiori dettagli (www.parlandosparlando.com/view.php/id_663/lingua_0/whoisit_1)
+runas #Il comando runas permette di assumere temporaneamente il profilo di un altro utente (magari quello di amministratore), senza dover interrompere la sessione di lavoro, di riaccedere a Windows con un altro profilo utente, di eseguire un task specifico e di tornare alla normale attivita' con il profilo di partenza.
 
 
-mem
-Il comando mem visualizza la quantita' di memoria usata e libera del sistema.
-MEM maggiori dettagli (www.parlandosparlando.com/view.php/id_664/lingua_0/whoisit_1)
+sort  #Il comando sort permette di ordinare alfabeticamente i contenuti di un file di testo e di inserirne il risultato in un nuovo file di testo.
 
 
-mkdir
-Il comando mkdir crea una nuova directory (cartella).
-MKdir maggiori dettagli (www.parlandosparlando.com/view.php/id_665/lingua_0/whoisit_1)
+subst #Il comando subst associa un percorso ad una lettera di unita'.
 
 
-more
-Il comando more visualizza i dati di output una schermata per volta.
-MORE maggiori dettagli (www.parlandosparlando.com/view.php/id_666/lingua_0/whoisit_1)
+sys #Il comando sys copia i file di sistema di MS-DOS e l'interprete dei comandi sul disco specificato.
 
 
-move
-Il comando move sposta file o cartelle su un nuovo percorso.
-MOVE maggiori dettagli (www.parlandosparlando.com/view.php/id_667/lingua_0/whoisit_1)
-
-
-path
-Il comando path visualizza o imposta un percorso di ricerca per i file eseguibili.
-PATH maggiori dettagli (www.parlandosparlando.com/view.php/id_668/lingua_0/whoisit_1)
-
-
-ping
-Il comando ping consente di verificare la comunicazione tra due computer di una rete.
-PING maggiori dettagli (www.parlandosparlando.com/view.php/id_680/lingua_0/whoisit_1)
-
-
-print
-Il comando print stampa un file di testo consentendo di usare altri comandi di MS-DOS.
-PRINT maggiori dettagli (www.parlandosparlando.com/view.php/id_669/lingua_0/whoisit_1)
-
-
-prompt
-Il comando prompt cambia il prompt dei comandi di MS-DOS.
-PROmPT maggiori dettagli (www.parlandosparlando.com/view.php/id_670/lingua_0/whoisit_1)
-
-
-rd
-Il comando rd rimuove una directory.
-RD maggiori dettagli (www.parlandosparlando.com/view.php/id_671/lingua_0/whoisit_1)
-
-
-recover
-Il comando recover permette di recuperare documenti andati perduti in settori corrotti del disco fisso (sempre che si abbia un file system NTFS).
-RECovER maggiori dettagli (www.parlandosparlando.com/view.php/id_681/lingua_0/whoisit_1)
-
-
-ren
-Il comando ren consente di rinominare file o cartelle.
-REN maggiori dettagli (www.parlandosparlando.com/view.php/id_672/lingua_0/whoisit_1)
-
-
-rename
-Il comando rename rinomina uno o piu' file.
-RENAME maggiori dettagli (www.parlandosparlando.com/view.php/id_672/lingua_0/whoisit_1)
-
-
-replace
-Il comando replace sostituisce i file.
-REPLACE maggiori dettagli (www.parlandosparlando.com/view.php/id_673/lingua_0/whoisit_1)
-
-
-rmdir
-Il comando rmdir rimuove una directory.
-RMdiR maggiori dettagli (www.parlandosparlando.com/view.php/id_671/lingua_0/whoisit_1)
-
-
-runas
-Il comando runas permette di assumere temporaneamente il profilo di un altro utente (magari quello di amministratore), senza dover interrompere la sessione di lavoro, di riaccedere a Windows con un altro profilo utente, di eseguire un task specifico e di tornare alla normale attivita' con il profilo di partenza.
-RUNAS maggiori dettagli (www.parlandosparlando.com/view.php/id_682/lingua_0/whoisit_1)
-
-
-sort
-Il comando sort permette di ordinare alfabeticamente i contenuti di un file di testo e di inserirne il risultato in un nuovo file di testo.
-SORT maggiori dettagli (www.parlandosparlando.com/view.php/id_683/lingua_0/whoisit_1)
-
-
-subst
-Il comando subst associa un percorso ad una lettera di unita'.
-SUBST maggiori dettagli (www.parlandosparlando.com/view.php/id_674/lingua_0/whoisit_1)
-
-
-sys
-Il comando sys copia i file di sistema di MS-DOS e l'interprete dei comandi sul disco specificato.
-SYS maggiori dettagli ()
-
-
-taskkill
-Il comando taskkill (se non si avvia digitare tskill anziche' taskkill) sostituisce il Task Manager di Windows e permette di eseguire operazioni di chiusura di task rapidissime.
-
-Utilizzandone le varie opzioni e' possibile, inoltre, intervenire su piu' programmi contemporaneamente, come per esempio nel caso delle applicazioni che non rispondono che possono essere chiuse tutte in un colpo solo digitando
+taskkill  #Il comando taskkill (se non si avvia digitare tskill anziche' taskkill) sostituisce il Task Manager di Windows e permette di eseguire operazioni di chiusura di task rapidissime.
+#Utilizzandone le varie opzioni e' possibile, inoltre, intervenire su piu' programmi contemporaneamente, come per esempio nel caso delle applicazioni che non rispondono che possono essere chiuse tutte in un colpo solo digitando
 taskkill /fi "status eq not responding"
 
-TASKKILL maggiori dettagli ()
 
 
-type
-Il comando type visualizza il contenuto di un file di testo.
-TYPE maggiori dettagli (www.parlandosparlando.com/view.php/id_675/lingua_0/whoisit_1)
+type    #Il comando type visualizza il contenuto di un file di testo.
 
 
-undelete
-Il comando undelete ripristina i file cancellati precedentemente con il comando del.
-UNdeLETE maggiori dettagli ()
+undelete  #Il comando undelete ripristina i file cancellati precedentemente con il comando del.
+unformat  #Il comando unformat ripristina i dati di un disco cancellati con il comando format.
 
 
-unformat
-Il comando unformat ripristina i dati di un disco cancellati con il comando format.
-UNfoRMAT maggiori dettagli ()
+ver   #Il comando ver visualizza la versione di MS-DOS in uso.
+vol #Il comando vol visualizza l'etichetta e il numero di serie di volume del disco.
 
 
-ver
-Il comando ver visualizza la versione di MS-DOS in uso.
-VER maggiori dettagli (www.parlandosparlando.com/view.php/id_676/lingua_0/whoisit_1)
-
-
-vol
-Il comando vol visualizza l'etichetta e il numero di serie di volume del disco.
-VOL maggiori dettagli (www.parlandosparlando.com/view.php/id_677/lingua_0/whoisit_1)
-
-
-xcopy
-Il comando xcopy cipia i file (ad esclusione dei file nascosti e di sistema) e la struttura delle directory (intere cartelle, sottocartelle incluse).
-XcoPY maggiori dettagli (www.parlandosparlando.com/view.php/id_678/lingua_0/whoisit_1)
+xcopy #Il comando xcopy cipia i file (ad esclusione dei file nascosti e di sistema) e la struttura delle directory (intere cartelle, sottocartelle incluse).
