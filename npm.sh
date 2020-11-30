716c5263e31c56483a665bce9b2cc6fcbad8afe8
JorgeFigueroa <atientas0412@gmail.com>
2020-11-30 05:09:12 +0100

    Create npm.sh

---
+++ npm.sh
@@ -0,0 +1,33 @@
+#https://www.javascripttutorial.net/nodejs-tutorial/npm-list/
+
+npm --version
+npm init 
+npm install packages 
+npm uninstall packages
+npm install package  --save-dev
+
+npm update package
+npm list 
+npm ls
+npm list --depth=0
+npm list --prod
+npm list --prod --depth=0
+npm list --dev
+npm list --global
+npm list --depth=0 --json
+
+#https://www.sitepoint.com/npm-guide/
+npm install -g npm@latest     #Updating npm
+npm config list
+npm config get prefix
+
+npm search mkdir
+npm cache clean --force
+
+npm i <package>: install local package
+npm i -g <package>: install global package
+npm un <package>: uninstall local package
+npm up: npm update packages
+npm t: run tests
+npm ls: list installed modules
+npm ll or npm la: print additional package information while listing modules
