342963593afab1e430412e6ceba019a8b259c1ee
JorgeFigueroa <atientas0412@gmail.com>
2020-12-07 02:29:27 +0100

    Update php_symfony3.sh

--- php_symfony3.sh
+++ php_symfony3.sh
@@ -48,9 +48,11 @@ bind-address = 127.0.0.1    #my.cnf:
 
 #https://xdebug.org/wizard --> WINDOWS
 Download php_xdebug-3.0.1-7.4-vc15-x86_64.dll
-Move the downloaded file to C:\php\ext
 Edit C:\php-7.4.6\php.ini and add the line
-zend_extension = C:\php\ext\php_xdebug-3.0.1-7.4-vc15-x86_64.dll
+[XDebug]
+zend_extension = C:\php-7.4.6\ext\php_xdebug-3.0.1-7.4-vc15-x86_64.dll
+xdebug.remote_enable = 1
+xdebug.remote_autostart = 1
 
 
 
