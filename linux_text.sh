8c466d8b06c28c7350c20d0d10688f762d9f5c9a
JorgeFigueroa <atientas0412@gmail.com>
2021-03-04 14:38:20 +0100

    Update linux_text.sh

--- linux_text.sh
+++ linux_text.sh
@@ -61,15 +61,41 @@ sort -r     #odernar de modo inverso
 unique      #muestra los unicos
 open
 
-https://www.guru99.com/the-vi-editor.html
+##https://www.guru99.com/the-vi-editor.html
 vi file_name
 esc   #modalita commando
 i     #modalita inserimento
 v     #modalita visuale
+
+w     #sposta parola succesiva
+b     #sposta parola precendente
+gg    #sposta prima riga
+G     #sposta ultima riga
+:100  #sposta riga 100
+
+x     #cancella caractere nel cursore
+dw    #cancella una parola
+d3w   #cancella 3 parole
+dd    #cancella riga
+3dd   #cancella 3 righe
+
+y     #copia caractere
+Y     #copia tutta la riga
+yw    #copia parola
+y3w   #copia 3 parole
+Y$    #copia fino a fine riga
+p     #incolla dopo il cursore
+P     #incolla prima il cursore
+
+/parola              #ricerca un parola
+/par.                #ricerca parte di una parola
+:s/testo/new_testo    #sostituice testo
+:s/testo//            #sostituice testo con psazio vouto  
+
 :w    #salva file
 :q    #chiudi file
 :wq   #salva e chiudi
-
+:q!   #forza la chiusura
 
 
 
