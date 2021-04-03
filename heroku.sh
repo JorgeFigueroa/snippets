34810e12b538ab684d446679041b6243cd6299d2
JorgeFigueroa <atientas0412@gmail.com>
2021-04-03 03:18:25 +0200

    Update heroku.sh

--- heroku.sh
+++ heroku.sh
@@ -4,3 +4,16 @@ brew tap heroku/brew && brew install heroku. #macOS
 
 
 heroku --version
+heroku login
+which heroku
+
+git init
+git add .
+git commit -m "init"
+
+heroku apps:create name-app
+heroku apps:create proyecto-mejorandola-prod --remote production
+heroku addons:add sendgrid --app proyecto-mejorandola-prod
+
+
+git push heroku master
