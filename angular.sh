d94c5c6af6fd654113cb0f29e2ac432f35187a88
JorgeFigueroa <atientas0412@gmail.com>
2021-04-24 16:56:30 +0200

    Create angular.sh

---
+++ angular.sh
@@ -0,0 +1,10 @@
+npm install -g @angular/cli
+ng new my-app
+ng serve –open
+
+ng g c --help  //ayuda
+ng g c pages/namecomponent  -is  // no css   --flat  // no carpeta    --spec=false  // no .spec
+ng g c home -it    //  no .html
+ng g s services/usuario 
+ng g g  guards/auth   // crea guards
+ng g m folder/nome_modulo –flat     // crea modulo
