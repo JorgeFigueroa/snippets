db0c86358478b5cb503cd05019632d668021e4db
JorgeFigueroa <atientas0412@gmail.com>
2021-05-10 12:01:58 +0200

    Update postgres.sh

--- postgres.sh
+++ postgres.sh
@@ -2,14 +2,27 @@
 #https://www.postgresql.org/docs/12/app-pg-ctl.html
 open cmd
 win C:\Program Files\PostgreSQL\12\bin 
+services.msc  postgres start
 osx  /Library/PostgreSQL/11/bin
 
-PostgreSQL\11\data\pg_hba.conf        #Client Authentication Configuration File
 PostgreSQL\11\data\postgresql.conf    #configuration file
   listen_addresses = '*'		# what IP address(es) to listen on;
   listen_addresses = 'IP1, IP2'	
   port = 5433				
   max_connections = 100			# (change requires restart)
+
+PostgreSQL\11\data\pg_hba.conf        #Client Authentication Configuration File
+  # TYPE  DATABASE        USER            ADDRESS                 METHOD
+  local   all             postgres        trust
+  # IPv4 local connections:
+  host    all             all             127.0.0.1/32            md5
+  # IPv6 local connections:
+  host    all             all             ::1/128                 md5
+  # Allow replication connections from localhost, by a user with the
+  # replication privilege.
+  host    replication     all             127.0.0.1/32            md5
+  host    replication     all             ::1/128                 md5
+  host    all             all             0.0.0.0/0               md5
 
 ./psql --version
 ./psql -h localhost -p 5432 -U postgres -W    #Connect to PostgreSQL database
