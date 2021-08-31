acdb17654132c8f7e423a74e13c9e28e4295e5b0
JorgeFigueroa <atientas0412@gmail.com>
2021-08-31 23:58:39 +0200

    Update linux_centos_lamp.sh

--- linux_centos_lamp.sh
+++ linux_centos_lamp.sh
@@ -45,4 +45,18 @@ java -jar file.jar
 nohup java -jar file.jar > /dev/null &
 
 
+env   #mostra variabili di ambiente
+echo $PATH
+
+###Cron e Schedulazioni
+ps aux|grep cron  #check process cron
+crontab -l        #list cron
+crontab -e        #edit cron
+0 */2 * ? * *  /path/script.sh >> /path/log.log
+
+0 */2 * ? * *	  #Every 2 minutes
+0 */30 * ? * *	#Every 30 minutes
+0 0 6 * * ?	    #Every day at 6am
+
+
 
