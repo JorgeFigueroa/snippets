232976d395c1c91928dc63e21f8ee2bfa9202464
JorgeFigueroa <atientas0412@gmail.com>
2021-10-19 00:50:22 +0200

    Update npm.sh

--- npm.sh
+++ npm.sh
@@ -2,17 +2,27 @@
 
 npm --version
 npm init --yes
-npm install packages 
-npm uninstall packages
+
+npm install packages
 npm install package  --save-dev
+npm i <package>: #install local package
+npm i -g <package>: #install global package
 
-npm pack
-npm install path_pack
+npm uninstall packages
+npm un <package>: #uninstall local package
+npm up #npm update packages
+npm t #run tests
+npm ll or npm la: #print additional packagenpm information while listing modules
 
+npm pack        #genera pachete zipado
+
+npm docs [package-name]   #Go to documentation page of package
+npm bugs [package-name]   #Go to issue page of package
+npm outdated              #List outdated packages
 
 npm update package
 npm list 
-npm ls
+npm ls    #List packages modules
 npm list --depth=0
 npm list --prod
 npm list --prod --depth=0
@@ -29,13 +39,6 @@ npm config get prefix
 npm search mkdir
 npm cache clean --force
 
-npm i <package>: install local package
-npm i -g <package>: install global package
-npm un <package>: uninstall local package
-npm up: npm update packages
-npm t: run tests
-npm ls: list installed modules
-npm ll or npm la: print additional package information while listing modules
 
 
 npm install -g nodemon # or using yarn: yarn global add nodemon
