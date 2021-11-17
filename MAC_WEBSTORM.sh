26ec34325d7db7c87a62fb0e63fca2f957960fb2
JorgeFigueroa <atientas0412@gmail.com>
2021-11-17 02:20:27 +0100

    Update MAC_WEBSTORM.sh

--- MAC_WEBSTORM.sh
+++ MAC_WEBSTORM.sh
@@ -1,14 +1,3 @@
-###### MAC
-cmd + alt N           #nueva finestra finder
-cmd + alt m           #para minimizar finestra
-cmd + alt w           #para cerrar todas las finestras
-cmd + shif 3          #para capturar screen 
-cmd + shif 4          #para seleccionar area
-cmd + shif 4 spazio   #captura una finestra especifica
-cmd + alt + i  #chrome svilupatore
-
-
-
 ######  WEBSTORM, PHPSTORM
 https://www.jetbrains.com/help/phpstorm/configuring-keyboard-and-mouse-shortcuts.html
 alt + click =     #para hacer una multiselecion
@@ -28,13 +17,3 @@ ALT + click     #VALOR EN ESE MOMENTO
 
 man
 
-
-defaults write com.apple.finder AppleShowAllFiles TRUE; killall Finder     #mostrar archivos ocultos
-defaults write com.apple.Finder AppleShowAllFiles NO; killall Finder      #ocultar archivos ocultos
-defaults write com.apple.screencapture type jpg; killall SystemUIServer      #cambiar el formato de captura de pantalla
-defaults write com.apple.screencapture type jpg;killall SystemUIServer      cambiar el formato de captura de pantalla
-
-apachectl stop		#parar apache
-Killall Dock      #reiniciar dock
-python -m SimpleHTTPServer 9000  #crea server
-sudo nano /private/etc/hosts
