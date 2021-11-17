3e44348b9f44d871626602bc20e17b0caa1338b2
JorgeFigueroa <atientas0412@gmail.com>
2021-11-17 02:19:40 +0100

    Update MAC_desktop_remoto.sh

--- MAC_desktop_remoto.sh
+++ MAC_desktop_remoto.sh
@@ -26,6 +26,30 @@ ipconfig /all
 	Indirizzo fisico. . . . . . . . . . . : EE-28-**********
 	Indirizzo IPv4. . . . . . . . . . . . : 192.168.1.118(Preferenziale)
 
-
 /usr/local/Cellar/wakeonlan/0.41/bin/wakeonlan -i 192.168.1.255 EE:28:********
 /usr/local/Cellar/wakeonlan/0.41/bin/wakeonlan -i 192.168.1.118 EE:28:********
+
+
+
+
+
+###### MAC
+cmd + alt N           #nueva finestra finder
+cmd + alt m           #para minimizar finestra
+cmd + alt w           #para cerrar todas las finestras
+cmd + shif 3          #para capturar screen 
+cmd + shif 4          #para seleccionar area
+cmd + shif 4 spazio   #captura una finestra especifica
+cmd + alt + i  #chrome svilupatore
+
+
+defaults write com.apple.finder AppleShowAllFiles TRUE; killall Finder     #mostrar archivos ocultos
+defaults write com.apple.Finder AppleShowAllFiles NO; killall Finder      #ocultar archivos ocultos
+defaults write com.apple.screencapture type jpg; killall SystemUIServer      #cambiar el formato de captura de pantalla
+defaults write com.apple.screencapture type jpg;killall SystemUIServer      cambiar el formato de captura de pantalla
+
+apachectl stop		#parar apache
+Killall Dock      #reiniciar dock
+python -m SimpleHTTPServer 9000  #crea server
+sudo nano /private/etc/hosts
+
