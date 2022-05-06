f63539d80697c1f0cadfcbd96da0d1a4e00a98aa
JorgeFigueroa <atientas0412@gmail.com>
2022-05-06 18:26:30 +0200

    Update linux_04_centos_lamp.sh

--- linux_04_centos_lamp.sh
+++ linux_04_centos_lamp.sh
@@ -58,12 +58,3 @@ du -hs  #Spazio occupato della cartella
 env   #mostra variabili di ambiente
 echo $PATH
 
-### Cron e Schedulazioni
-ps aux|grep cron  #check process cron
-crontab -l        #list cron
-crontab -e        #edit cron
-0 */2 * ? * *  /path/script.sh >> /path/log.log
-
-0 */2 * ? * *	  #Every 2 minutes
-0 */30 * ? * *	#Every 30 minutes
-0 0 6 * * ?	    #Every day at 6am
