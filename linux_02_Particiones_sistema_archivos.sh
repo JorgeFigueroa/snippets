8246ccc5e7211260d43ea4883861c74de858d05e
JorgeFigueroa <atientas0412@gmail.com>
2022-05-06 18:20:22 +0200

    Create linux_02_Particiones_sistema_archivos.sh

---
+++ linux_02_Particiones_sistema_archivos.sh
@@ -0,0 +1,10 @@
+which dmesg
+man dmesg
+dmesg	 # print or control the kernel ring buffer
+
+which df
+man df
+df -h	# report file system disk space usage
+
+man fdisk
+sudo fdisk -l  # manipulate disk partition table
