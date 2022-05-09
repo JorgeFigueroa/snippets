6b2381d509f825f9b6748e623eff94718d9bb9cb
JorgeFigueroa <atientas0412@gmail.com>
2022-05-09 14:47:44 +0200

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -103,6 +103,8 @@ openssl genrsa -out ca.key 2084 #Generar Llave privada
 openssl req -new -key ca.key -out ca.csr	#Generar Archivo CSR 
 openssl x509 -req -days 90 -in ca.csr -signkey ca.key -out ca.crt	#Generar Certificado 
 
+openssl req -nodes -x509 -newkey rsa:4096 -keyout server.key -out server.crt -days 365 -subj /CN=nomedominio.com
+
 #C:\Apache24\conf\httpd.conf
 LoadModule ssl_module modules/mod_ssl.so
 LoadModule socache_shmcb_module modules/mod_socache_shmcb.so
