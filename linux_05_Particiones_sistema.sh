9e3f39fc5ea0955efd60287151bf1764035330a7
JorgeFigueroa <atientas0412@gmail.com>
2022-11-09 02:13:19 +0100

    Update linux_05_Particiones_sistema.sh

--- linux_05_Particiones_sistema.sh
+++ linux_05_Particiones_sistema.sh
@@ -21,5 +21,6 @@ du -hs  #Spazio occupato della cartella
 
 
 netstat -tnlp | grep :22222
+netstat -tnlp | grep apache
 netstat -na | grep 8080 | grep ESTABLISHED # view user conected
 netstat -na | grep 443 | grep ESTABLISHED  # view user conected
