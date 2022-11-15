16486e4ba0d911b89b20ddc5b5dfa133bed0cc4e
JorgeFigueroa <atientas0412@gmail.com>
2022-11-15 01:30:09 +0100

    Create aws.sh

---
+++ aws.sh
@@ -0,0 +1,5 @@
+https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
+$ aws configure
+AWS Access Key ID [None]: ***************
+AWS Secret Access Key [None]: **********************
+Default region name [None]: us-west-2
