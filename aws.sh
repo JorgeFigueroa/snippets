f07b9a497dd3a650156ca38f05aa0a00ad26069d
JorgeFigueroa <atientas0412@gmail.com>
2022-11-15 02:01:16 +0100

    Update aws.sh

--- aws.sh
+++ aws.sh
@@ -9,6 +9,7 @@ Default region name [None]: us-west-2
 https://www.serverless.com/framework/docs/getting-started
 npm install -g serverless
 npm init -y
+serverless -v
 serverless config credentials --provider aws --profile strudent1 --key *********** --secret *************
 serverless create --template aws-nodejs --name name_project
 serverless deploy --verbose
