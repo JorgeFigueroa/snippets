d136df9a474e50fb221d90d38c7255e1a20c5f1f
JorgeFigueroa <atientas0412@gmail.com>
2022-11-22 11:41:17 +0100

    Update linux_09_crontab.sh

--- linux_09_crontab.sh
+++ linux_09_crontab.sh
@@ -1,7 +1,12 @@
+sudo apt install cron
+sudo systemctl enable --now cron
+ls -l /var/spool/cron/crontabs
+
 ### Cron e Schedulazioni
 ps aux|grep cron  #check process cron
 crontab -l        #list cron
 crontab -e        #edit cron
+crontab -u root -e
 0 */2 * ? * *  /path/script.sh >> /path/log.log
 
 0 */2 * ? * *	  #Every 2 minutes
