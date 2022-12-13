be359f8986154ebc3d7cebd43be687a157f55469
JorgeFigueroa <atientas0412@gmail.com>
2022-12-13 15:52:53 +0100

    Update python.sh

--- python.sh
+++ python.sh
@@ -34,3 +34,25 @@ venv/Script/activate  ## win
 
 ### How To Install Python 3.10 on Ubuntu 20.04|18.04
 https://computingforgeeks.com/how-to-install-python-on-ubuntu-linux-system/
+sudo apt update
+sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
+wget https://www.python.org/ftp/python/3.10.9/Python-3.10.9.tgz
+tar -xf Python-3.10.*.tgz
+cd Python-3.10.*/
+./configure --enable-optimizations
+make -j $(nproc)
+sudo make altinstall
+python3.10 --version
+
+
+
+
+
+
+
+
+
+
+
+
+
