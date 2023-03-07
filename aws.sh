afaf2a5326f718505b725c6d7aab4e4e177520af
JorgeFigueroa <atientas0412@gmail.com>
2023-03-07 22:26:57 +0100

    Update aws.sh

--- aws.sh
+++ aws.sh
@@ -2,7 +2,7 @@ https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
 $ aws configure
 AWS Access Key ID [None]: ***************
 AWS Secret Access Key [None]: **********************
-Default region name [None]: us-west-2
+Default region name [None]: eu-west-1
 
 aws configure list
 
