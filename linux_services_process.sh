716954335cd6c33441822bd72b6cd60276aad36b
JorgeFigueroa <atientas0412@gmail.com>
2023-05-26 04:29:13 +0200

    Update linux_services_process.sh

--- linux_services_process.sh
+++ linux_services_process.sh
@@ -1,14 +1,8 @@
 systemctl list-unit-files --type=service
 systemctl list-unit-files --type=service | grep enabled
 
-systemctl disable <nombre_del_servicio>
-systemctl enable <nombre_del_servicio>
-systemctl start <nombre_del_servicio>
+systemctl disable /enable / start <nombre_del_servicio>
 
-top 1	                #mostrar procesos
-ps -fe | grep res     #filtar processi
-ps -A | wc -l         #contar procesos
-ps fax                #ver procesos
 
 
 /etc/security/limits.conf > user hard nproc 300
