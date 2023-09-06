838c6026a39baccb96d27f201dce70f9173df136
JorgeFigueroa <atientas0412@gmail.com>
2023-09-06 11:05:39 +0200

    Update nodejs_nginx.sh

--- nodejs_nginx.sh
+++ nodejs_nginx.sh
@@ -18,6 +18,8 @@ nvm install [version]        # Download and install [version]
 nvm uninstall [version]      # Uninstall [version]
 nvm use [version]            # Switch to use [version]
 nvm list                     # List installed versions
+nvm list available           # List all version 
+
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 source ~/.profile
