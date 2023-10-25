2ecb5abbfde0bf37db5d22a920a5dde82c66b7e0
JorgeFigueroa <atientas0412@gmail.com>
2023-10-25 16:55:11 +0200

    Update aws_policy.sh

--- aws_policy.sh
+++ aws_policy.sh
@@ -1,5 +1,6 @@
+ROLE --> POLICY --> Statement []
+
 aws iam list-users
-Role --> POLICY permissions
 {
     "Version": "2012-10-17",
     "Statement": [
@@ -8,6 +9,21 @@ Role --> POLICY permissions
             "Effect": "Allow",
             "Action": "iam:ListUsers",
             "Resource": "*"
+        }
+    ]
+}
+
+aws dynamodb list-tables --region eu-south-1
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "DynamoDBListAllTables",
+            "Effect": "Allow",
+            "Action": [
+                "dynamodb:ListTables"
+            ],
+            "Resource": "arn:aws:dynamodb:eu-south-1:162229746485:table/*"
         }
     ]
 }
