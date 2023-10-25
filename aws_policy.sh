3231f9fabace752ad482b68e829e8e31154b4f2d
JorgeFigueroa <atientas0412@gmail.com>
2023-10-25 13:07:54 +0200

    Create aws_policy.sh

---
+++ aws_policy.sh
@@ -0,0 +1,13 @@
+aws iam list-users
+Role --> POLICY permissions
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": "iam:ListUsers",
+            "Resource": "*"
+        }
+    ]
+}
