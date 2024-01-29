a0fec7171a8ac014bd1a45a3ff85b75f8dc5e18e
JorgeFigueroa <atientas0412@gmail.com>
2024-01-29 13:10:38 +0100

    Update google.sh

--- google.sh
+++ google.sh
@@ -1,6 +1,6 @@
 filetype:pdf nombre libro
 site:drive.google.com nombre libro
 
-site:wikipedia.org biología evolutiva
+site:wikipedia.org palabra
 related:amazon.com
-define:filantropía
+define:palabra
