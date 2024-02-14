37a234ec4c9008677e3245d58e554afe80bd5b45
JorgeFigueroa <atientas0412@gmail.com>
2024-02-14 01:02:17 +0100

    Update linux_02_usuarios_permisos.sh

--- linux_02_usuarios_permisos.sh
+++ linux_02_usuarios_permisos.sh
@@ -14,10 +14,11 @@ whoami #nombre usuario
 groups #nombre  grupo
 id  #info usuario
 
+apt-get update
+apt-get install adduser
 export PATH=$PATH:/usr/sbin:/sbin  #agregar /usr/sbin y /sbin a tu PATH de manera temporal
 useradd name_user -d /home/name_user -s /bin/bash -G name_gruppo -g gruppo_default # Crea user con Directorio Home, Shell, y Grupos Especificados
-## Crea user sin Directorio Home, con Acceso de No-Login - para cuentas de servicio o procesos, no para una sesión interactiva.
-useradd user_nologin -M -s /bin/nologin -G name_gruppo -g gruppo_default   
+useradd user_nologin -M -s /bin/nologin -G name_gruppo -g gruppo_default   ## Crea user sin Directorio, con Acceso de No-Login para cuentas de servicio o procesos, no para una sesión interactiva.
 
 useradd -m name_user            # create user
 usermod -aG sudo name_user      # add user to group admin
@@ -29,8 +30,6 @@ passwd name_user  tu_pass       # change pasword
 groupadd name_group 
 groupdel name_group
 groupmod name_group -g 1002
-
-
 
 
 ### PERMESSI FILE E CARTELLE
