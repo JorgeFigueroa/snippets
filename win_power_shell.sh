ae2f9fbdf366fa747d47deba519ffa99adea1884
JorgeFigueroa <atientas0412@gmail.com>
2024-02-17 19:31:28 +0100

    Update win_power_shell.sh

--- win_power_shell.sh
+++ win_power_shell.sh
@@ -1,8 +1,10 @@
+## LINUX
+du -sh ./* | sort -hr
+
 ## mostra il peso delle cartelle
 Get-ChildItem | Where-Object { $_.PSIsContainer } | ForEach-Object { $_.Name + " : " + "{0:N2}" -f ((Get-ChildItem $_.FullName -Recurse | Measure-Object -Property Length -Sum).Sum / 1MB) + " MB" }
 
-## LINUX
-du -sh ./* | sort -hr
+
 
 
 ## Ver el espacio ocupado por las carpetas en la ubicación actual, ordenadas por tamaño
