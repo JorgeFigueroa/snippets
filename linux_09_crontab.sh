01def9e28477bf34467b19607fff281bd4356b23
JorgeFigueroa <atientas0412@gmail.com>
2024-02-19 16:08:24 +0100

    Update linux_09_crontab.sh

--- linux_09_crontab.sh
+++ linux_09_crontab.sh
@@ -3,6 +3,10 @@ systemctl enable --now cron
 ls /var/spool/cron/crontabs
 
 export VISUAL=nano; crontab -e
+sudo EDITOR=nano crontab -e
+sudo VISUAL=nano crontab -e
+
+
 
 ### Cron e Schedulazioni
 ps aux|grep cron  #check process cron
@@ -23,3 +27,13 @@ minute(s) hour(s) day(s) month(s) weekday(s) command(s)
 0 */2 * ? * *	  #Every 2 minutes
 0 */30 * ? * *	#Every 30 minutes
 0 0 6 * * ?	    #Every day at 6am
+
+
+* * * * * comando_a_ejecutar
+- - - - -
+| | | | |
+| | | | +---- Día de la semana (0 - 7) (Domingo es 0 o 7)
+| | | +------ Mes (1 - 12)
+| | +-------- Día del mes (1 - 31)
+| +---------- Hora (0 - 23)
++------------ Minuto (0 - 59)
