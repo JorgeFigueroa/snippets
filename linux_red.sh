d41c62bcbb8b5b9ace0642c9261e8ef3876fe85d
JorgeFigueroa <atientas0412@gmail.com>
2024-02-19 15:28:51 +0100

    Update linux_red.sh

--- linux_red.sh
+++ linux_red.sh
@@ -2,10 +2,16 @@ iptables -L -n -v  #1. Displaying the Status of Your Firewall  https://www.cyber
 
 ssh user@IP    #conectar a servidor
 ssh -i key.pem user@IP -pNUMERO_PORT
-scp -P PORT -r -i path/key.pem /path_local user@destination:/path_destination
+scp -P PORT -r -i path/key.pem /path_local user@IP_REMOTE:/path_destination
+scp -r path_local file1.json file2.md user@IP_REMOTE:/path_destination
+scp -r src  *.{md,json} user@IP_REMOTE:/path_destination
+
 rsync -rv user@IP:/var/www/build/path/vendor .
 rsync -avz -e "ssh -i  key.pem -p PORT" /path_local  user@IP:/path_destination
+rsync -av -e "ssh -i  key.pem -p PORT" --exclude='file.txt' user@IP:/path_destination
 
+
+ 
 ftp hostname
 
 ping 192.168.1.1   #comprobar IP esta respondiendo
