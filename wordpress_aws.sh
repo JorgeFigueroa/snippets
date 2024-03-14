b6c3fcf98c330baae5f053bf4988651060945944
JorgeFigueroa <atientas0412@gmail.com>
2024-03-14 01:51:09 +0100

    Update wordpress_aws.sh

--- wordpress_aws.sh
+++ wordpress_aws.sh
@@ -1,6 +1,6 @@
 ## AMI WORDPRESS BITNAMI
 https://docs.bitnami.com/aws/faq/get-started/find-credentials/#option-2-find-password-by-connecting-to-your-application-through-ssh
-ssh -i "EC2_GIORGIOMILANO.pem" bitnami@3.252.253.225
+ssh -i "CHIAVE.pem" bitnami@IP
 sudo cat /home/bitnami/bitnami_credentials
 mysql -u root -p  ## pass  bitnami_credentials 
 
