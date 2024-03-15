c63a22d644a974b98139aff24d7835fffffea609
JorgeFigueroa <atientas0412@gmail.com>
2024-03-15 17:53:47 +0100

    Update aws_bastion_host.sh

--- aws_bastion_host.sh
+++ aws_bastion_host.sh
@@ -1,27 +1,31 @@
+## CONECT SSM TO EC2 ISTANZE
+aws ssm start-session --target ID_ISTANZE --region eu-west-1
+
 ## crea key privata, in aws carica la key publica
 aws ec2 create-key-pair --key-name MiKeyPair
 
 ## create security group
-aws ec2 create-security-group --group-name MySecurityGroup --description "Security group for SSH access" --vpc-id vpc-abcdef123
-aws ec2 authorize-security-group-ingress --group-id sg-xxxxxx --protocol tcp --port 22 --cidr 0.0.0.0/0
+aws ec2 create-security-group --group-name MySecurityGroup --description "Security group for bastion host" --vpc-id VPC_ID
+aws ec2 authorize-security-group-ingress --group-id ID_SG --protocol tcp --port 22 --cidr 0.0.0.0/0
 
-## create istanze subnet publica
-aws ec2 run-instances \
-    --image-id ami-1234567890abcdef0 \
-    --count 1 \
-    --instance-type t2.micro \
-    --key-name MyKeyPair \
-    --subnet-id subnet-publica \
-    --security-group-ids sg-0123456789abcdef0 \
-    --block-device-mappings '[
-        {
-            "DeviceName": "/dev/sdh",
-            "Ebs": {
-                "VolumeSize": 8,
-                "VolumeType": "gp2",
-                "DeleteOnTermination": true
-            }
-        }
-    ]'
+## create role con policy
+aws iam create-role --role-name SSMInstanceRole --assume-role-policy-document file://trust-policy.json
+aws iam attach-role-policy --role-name SSMInstanceRole --policy-arn arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore
 
-aws ec2 run-instances --image-id ami-1234567890abcdef0 --count 1 --instance-type t2.micro --key-name MyKeyPair --subnet-id subnet-PRIVATA --security-group-ids sg-0123456789abcdef0
+aws iam create-instance-profile --instance-profile-name SSMInstanceProfile
+aws iam add-role-to-instance-profile --instance-profile-name SSMInstanceProfile --role-name SSMInstanceRole
+
+## create istanze BASTIONHOST
+aws ec2 associate-iam-instance-profile --instance-id ID_ISTANZE --iam-instance-profile Arn="ARN_SSMInstanceProfile"
+aws ec2 run-instances --image-id ID_AMI --count 1 --instance-type t2.micro  --subnet-id IS_SUBNET --security-group-ids ID_SG --iam-instance-profile Name=SSMInstanceRole
+
+
+
+
+## CHECK ARN
+aws iam get-role --role-name SSMInstanceRole --query 'Role.Arn' --output text
+aws iam get-instance-profile --instance-profile-name SSMInstanceProfile --query 'InstanceProfile.Arn' --output text
+
+## CHECK SSM ISTANZE EC2
+sudo systemctl status amazon-ssm-agent
+
