966c5403cbea1697de843dcdbdcd4090315d1792
JorgeFigueroa <atientas0412@gmail.com>
2024-03-15 16:14:33 +0100

    Update aws_bastion_host.sh

--- aws_bastion_host.sh
+++ aws_bastion_host.sh
@@ -1,12 +1,17 @@
+## crea key privata, in aws carica la key publica
+aws ec2 create-key-pair --key-name MiKeyPair
+
+## create security group
 aws ec2 create-security-group --group-name MySecurityGroup --description "Security group for SSH access" --vpc-id vpc-abcdef123
 aws ec2 authorize-security-group-ingress --group-id sg-xxxxxx --protocol tcp --port 22 --cidr 0.0.0.0/0
 
+## create istanze subnet publica
 aws ec2 run-instances \
     --image-id ami-1234567890abcdef0 \
     --count 1 \
     --instance-type t2.micro \
     --key-name MyKeyPair \
-    --subnet-id subnet-abcdef12 \
+    --subnet-id subnet-publica \
     --security-group-ids sg-0123456789abcdef0 \
     --block-device-mappings '[
         {
@@ -19,3 +24,4 @@ aws ec2 run-instances \
         }
     ]'
 
+aws ec2 run-instances --image-id ami-1234567890abcdef0 --count 1 --instance-type t2.micro --key-name MyKeyPair --subnet-id subnet-PRIVATA --security-group-ids sg-0123456789abcdef0
