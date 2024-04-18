26381481804270d1c2e8f1a84c095071ca5c2346
JorgeFigueroa <atientas0412@gmail.com>
2024-04-18 23:27:13 +0200

    Update win_power_shell.sh

--- win_power_shell.sh
+++ win_power_shell.sh
@@ -1,6 +1,11 @@
 ## LINUX
 du -sh ./* | sort -hr
 
+
+start .    # open in explorer
+F:         # cambia unita
+tree /F    # windows
+
 ## mostra il peso delle cartelle
 Get-ChildItem | Where-Object { $_.PSIsContainer } | ForEach-Object { $_.Name + " : " + "{0:N2}" -f ((Get-ChildItem $_.FullName -Recurse | Measure-Object -Property Length -Sum).Sum / 1MB) + " MB" }
 
