91c51432fdd3c8a42b74a2da57e7ed0e3706fd58
JorgeFigueroa <atientas0412@gmail.com>
2024-04-19 15:32:36 +0200

    Update win_power_shell.sh

--- win_power_shell.sh
+++ win_power_shell.sh
@@ -14,3 +14,6 @@ Get-ChildItem | Where-Object { $_.PSIsContainer } | ForEach-Object { $_.Name + "
 
 ## Ver el espacio ocupado por las carpetas en la ubicación actual, ordenadas por tamaño
 Get-ChildItem -Directory | ForEach-Object { $_.Name + " : " + "{0:N2} MB" -f ((Get-ChildItem $_.FullName -Recurse | Measure-Object -Property Length -Sum).Sum / 1MB) } | Sort-Object { [decimal]($_ -split ': ')[1] } -Descending
+
+# cmd svuota cestino come amministrator
+rd /s /q C:\$Recycle.bin
