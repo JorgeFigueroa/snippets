a4dc6214c639c055e219ac304a5e55e1b0a26d1f
JorgeFigueroa <atientas0412@gmail.com>
2024-04-24 00:31:52 +0200

    Update linux_ubuntu.sh

--- linux_ubuntu.sh
+++ linux_ubuntu.sh
@@ -1,1 +1,17 @@
+## Actualizar la lista de paquetes:
+sudo apt update
+sudo apt upgrade
+
+## Instalar Python 3.12
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install python3.12
+
+# Instalar Java 8
+sudo apt install openjdk-8-jdk
+
+# Instalar PHP 7.3
+sudo add-apt-repository ppa:ondrej/php
+sudo apt update
+sudo apt install php7.3
 
